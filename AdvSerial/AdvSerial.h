b45e76c88fca2067205a2ca67f3de9cefee483e4
Xachaeus <77939788+Xachaeus@users.noreply.github.com>
2021-01-25 18:38:13 -0500

    Add files via upload

--- AdvSerial/AdvSerial.h
+++ AdvSerial/AdvSerial.h
@@ -16,4 +16,40 @@ private:
   char _end_char;
   String _backgroundString;
 };
+
+AdvSerial::AdvSerial(char end_char)
+{
+  _end_char = end_char;
+}
+
+String AdvSerial::Read()
+{
+  bool ended = false;
+  String returnString = "";
+  
+  while (not ended)
+  {
+    while (not Serial.available()){}
+    char in = Serial.read();
+    if(in == _end_char) { return returnString; ended = true;}
+    returnString += in;
+    
+  }
+}
+
+String AdvSerial::Background_Read()
+{
+  while (Serial.available())
+  {
+    char in = Serial.read();
+    if (in == _end_char)
+    {
+      String temp = _backgroundString;
+      _backgroundString = "";
+      return temp;
+    }
+    _backgroundString += in;
+  }
+  return "";
+}
 #endif
